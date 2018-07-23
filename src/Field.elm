85514d5319db3c55d418d83e289cd1bab7635443
breezyboa <blitzritz@gmail.com>
2018-07-23 14:25:39 +0530

    Add ValidationResult type

--- src/Field.elm
+++ src/Field.elm
@@ -1,9 +1,10 @@
-module Field exposing (Field(..), value, update, validate, validateAll, isValid, isAllValid, map, andThen)
+module Field exposing (Field(..), value, update, validate, validateAll, isValid, isAllValid, map)
 
-{-| A Field is a simple data type that helps capture and validate form data better.
+{-| A `Field` is a simple data type that helps capture and validate form data better.
 The left side of a field represents a function that takes in a value of arbitrary type,
-validates it and returns a tuple consisting of a Maybe String and the actual value of the field.
-The Maybe String can represting an fnor message if the validation fails
+validates it and returns a `ValidationResult`.
+The `ValidationResult` can contain the value of the field if the validation `Passed` or
+an error if the validation `Failed`
 
 
 # Definition
@@ -20,81 +21,71 @@ The Maybe String can represting an fnor message if the validation fails
 
 @docs map
 
-
-# Chaining validations
-
-@docs andThen
-
 -}
+
+import ValidationResult exposing (..)
 
 
 {-| A simple data type that helps capture and validate form data better
 -}
-type Field a
-    = Field (a -> ( Maybe String, a )) a
+type Field e v
+    = Field (v -> ValidationResult e v) v
 
 
 {-| Extract the value out of a Field
 -}
-value : Field a -> a
+value : Field e v -> v
 value (Field fn value) =
     value
 
 
 {-| Update the value of a Field
 -}
-update : Field a -> a -> Field a
+update : Field e v -> v -> Field e v
 update (Field fn value) v =
     Field fn v
 
 
-{-| Validate a Field and get a Maybe String which may contain an error message
+{-| Validate a Field and get a ValidationResult
 -}
-validate : Field a -> Maybe String
+validate : Field e v -> ValidationResult e v
 validate (Field fn value) =
-    Tuple.first <| fn value
+    fn value
 
 
-{-| Validate a List of Fields and get a List of Maybe String which may contain error messages for all fields
+{-| Validate a List of Fields and get a List of ValidationResult
 -}
-validateAll : List (Field a) -> List (Maybe String)
+validateAll : List (Field e v) -> List (ValidationResult e v)
 validateAll =
     List.map validate
 
 
 {-| Check if a Field is valid
 -}
-isValid : Field a -> Bool
-isValid f =
-    case validate f of
-        Just _ ->
+isValid : Field e v -> Bool
+isValid =
+    isValid_ << validate
+
+
+isValid_ : ValidationResult e v -> Bool
+isValid_ f =
+    case f of
+        Failed _ ->
             False
 
-        Nothing ->
+        Passed _ ->
             True
 
 
 {-| Check if all Fields in the list are valid
 -}
-isAllValid : List (Field a) -> Bool
+isAllValid : List (Field e v) -> Bool
 isAllValid f =
-    List.all ((==) Nothing) <| validateAll f
+    List.all (isValid_) <| validateAll f
 
 
 {-| Map over a Field
 -}
-map : (a -> a) -> Field a -> Field a
+map : (v -> v) -> Field e v -> Field e v
 map mapFn (Field fn value) =
     Field fn (mapFn value)
-
-
-{-| Chain multiple validation functions
--}
-andThen : (a -> ( Maybe String, a )) -> ( Maybe String, a ) -> ( Maybe String, a )
-andThen fn t =
-    case t of
-        ( Just err, v ) ->
-            ( Just err, v )
-
-        ( Nothing, v ) ->
-            fn v
---
+++ src/ValidationResult.elm
@@ -0,0 +1,67 @@
+module ValidationResult exposing (ValidationResult(..), map, contraMap, andThen)
+
+{-|
+
+
+# Definition
+
+@docs ValidationResult
+
+
+# Mapping
+
+@docs map, contraMap
+
+
+# Chaining
+
+@docs andThen
+
+-}
+
+
+{-| A `ValidationResult` describes the state of a `Field` once it has been validated.
+It can contain the value of the field if the validation `Passed` or
+an error if the validation `Failed`
+-}
+type ValidationResult e v
+    = Passed v
+    | Failed e
+
+
+{-| Apply a function to a `ValidationResult`. If the `ValidationResult` is `Passed`, it will be converted.
+if the `ValidationResult` has `Failed` then the failure will propogate
+-}
+map : (a -> b) -> ValidationResult x a -> ValidationResult x b
+map fn v =
+    case v of
+        Failed err ->
+            Failed err
+
+        Passed val ->
+            Passed <| fn val
+
+
+{-| Apply a function to a `ValidationResult`. If the `ValidationResult` is `Failed`, it will be converted.
+if the `ValidationResult` has `Passed` then the passed state will propogate
+-}
+contraMap : (a -> b) -> ValidationResult a x -> ValidationResult b x
+contraMap fn e =
+    case e of
+        Failed err ->
+            Failed <| fn err
+
+        Passed val ->
+            Passed val
+
+
+{-| Chain multiple validation functions
+-}
+andThen : (v -> ValidationResult e v) -> ValidationResult e v -> ValidationResult e v
+andThen fn t =
+    case t of
+        Failed err ->
+            Failed err
+
+        Passed v ->
+            fn v
